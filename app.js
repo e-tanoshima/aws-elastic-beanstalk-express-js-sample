8fe2b8fe55f673b5f2e630bf1ad4def8004162e9
e-tanoshima <Tanoshima.Eiji@dpula.co.jp>
2022-01-02 08:45:02 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World in AWS!'));
+app.get('/', (req, res) => res.send('Hello World in AWS need to be commited!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
